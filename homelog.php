4c89c1d4b0f8ae09a28ef4aa438203453923407e
Lilian <lviennet@gaming.tech>
2022-11-29 18:23:36 +0100

    zzz

--- homelog.php
+++ homelog.php
@@ -1,4 +1,3 @@
-<?php require_once "cfg/config.php"; ?>
 <html>
  <head>
     <!--Import Google Icon Font-->
@@ -16,11 +15,11 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@700;800&display=swap" rel="stylesheet">
-    <link rel="Icon" href="img/logo-gc.jpg">
     <title>Connexion</title>
  </head>
  <body>
-    
+  
+
  <div class="navbar-fixed">
   <nav>
       <div class="nav-wrapper">
@@ -37,19 +36,21 @@
     <li class="switch"><label><input type="checkbox"><span class="lever"></span></label></li>
   </ul>
 
-    <h2 class="black-text-css" >Inscription</h2>
+    <h2 class="black-text-css form-title" >Inscription</h2>
     <form class="form-style" method="post" action="actions/signup.php">
         <input type='email' name='email' placeholder='email'/>
         <input type='text' name='username' placeholder='username'/>
         <input type='password' name='password' placeholder='password'/>
         <input type='submit' value='Créer un compte' />
     </form>
-  <h2 class="black-text-css">Connexion</h2>
+  <h2 class="black-text-css form-title">Connexion</h2>
   <form class="form-style" method="post" action="actions/login.php">
     <input type='text' name='username' placeholder='username'/>
     <input type='password' name='password' placeholder='password'/>
     <input type='submit' value='Me connecter' />
   </form>
+
+
 
 <!-- JQuery -->
 <script type="text/javascript" src="js/jquery.min.js"></script>
